3cd1ca1c966fb7ae1033386b34493a855a1321c6
Alexandr Aleynikov <alexdon201203@gmail.com>
2017-10-03 14:02:00 +0300

    где-то я не описал ЕдитТекст

--- app/src/main/java/com/example/fujick/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/fujick/myapplication1/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.fujick.myapplication1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.net.Uri;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+EditText link1;
+    String per;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +22,13 @@ public class MainActivity extends AppCompatActivity {
         (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ukr.net")));
+
+                link1=findViewById(R.id.link);
+                //per=getString(link1);
+                per=link1.getText().toString();
+
+               // Toast.makeText(getApplicationContext(),per, Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(per)));
             }
         });
     }
