7780403109ce0e85c9ebe3f5b2251c2fdd717e65
bondarenko.sergey <bondarenko.sergey.v@gmail.com>
2017-10-03 21:44:49 +0300

    [ref] Код ревью (рефакторинг).

--- app/src/main/java/com/example/fujick/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/fujick/myapplication1/MainActivity.java
@@ -1,39 +1,44 @@
 package com.example.fujick.myapplication1;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.net.Uri;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity {
-    EditText link1;
-    String per;
+
+    EditText urlInputField;  // old - urlInputField
+    Button goButton; // для читаемости кода стоит определить все используемые объекты
+
+    String url; // old name - url - давай объектам осмысленные имена
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        (findViewById(R.id.button)).setOnClickListener(new View.OnClickListener() {
+        // инициализируй объекты один раз - findViewById дорогая операция
+        goButton = findViewById(R.id.button);
+        urlInputField = findViewById(R.id.link);
+
+        goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                link1 = findViewById(R.id.link);
-                //per=getString(link1);
-                per = link1.getText().toString();
+                // urlInputField = findViewById(R.id.link);
+                url = urlInputField.getText().toString();
 
-                // Toast.makeText(getApplicationContext(),per, Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(per)));
+                // обрезал пустые символы (пробелы) с начала и конца строки, проверил, что остаток не пустая строка
+                if (!url.trim().isEmpty()) {
+
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                }
             }
         });
     }
 
 }
-
-
